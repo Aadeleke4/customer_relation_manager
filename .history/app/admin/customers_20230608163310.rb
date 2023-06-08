eb83587e63de0d38e0d00f3b5ac466836c612f47
Maryam Ayemlo Gambo <82781910+MaryamGambo@users.noreply.github.com>
2023-06-08 16:43:34 -0500

    installed active storage and added it to project

---
+++ .history/app/admin/customers_20230608163310.rb
@@ -0,0 +1,30 @@
+ActiveAdmin.register Customer do
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
+
+  form do |f|
+    f.inputs 'Customer Details' do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email_address
+      f.input :notes
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :full_name
+      row :phone_number
+      row :email_address
+      row :notes
+      row :image do |customer|
+        if customer.image.attached?
+          image_tag customer.image.variant(resize_to_limit: [100, 100])
+        else
+          'No image uploaded'
+        end
+      end
+    end
+  end
+end
