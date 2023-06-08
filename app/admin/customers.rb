6348db664849c7f2ad7c85248900dfbf0ae7c423
Maryam Ayemlo Gambo <82781910+MaryamGambo@users.noreply.github.com>
2023-06-08 16:42:15 -0500

    configured the active admin to allow image uploads

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Customer do
   actions :index, :edit, :update, :create, :destroy
-  permit_params :full_name, :phone_number, :email_address, :notes
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
 
   form do |f|
     f.inputs 'Customer Details' do
@@ -8,6 +8,7 @@ ActiveAdmin.register Customer do
       f.input :phone_number
       f.input :email_address
       f.input :notes
+      f.input :image, as: :file
     end
     f.actions
   end
@@ -18,7 +19,13 @@ ActiveAdmin.register Customer do
       row :phone_number
       row :email_address
       row :notes
-
+      row :image do |customer|
+        if customer.image.attached?
+          image_tag customer.image.variant(resize_to_limit: [100, 100])
+        else
+          'No image uploaded'
+        end
+      end
     end
   end
 end
